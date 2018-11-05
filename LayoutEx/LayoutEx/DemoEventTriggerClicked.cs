9f0efeab36eee6b3546595a802e9f3b4fb1beaf0
demorila <hoang.zn99@gmail.com>
2018-11-05 14:09:59 +0700

    delete bin

--- LayoutEx/LayoutEx/DemoEventTriggerClicked.cs
+++ LayoutEx/LayoutEx/DemoEventTriggerClicked.cs
@@ -12,12 +12,12 @@ namespace LayoutEx
         {
             await Task.WhenAll(
                 sender.ColorTo(sender.BackgroundColor, Color.Blue, c => sender.BackgroundColor = c, 2000),
-                sender.ScaleTo(.7, 2000),
-                sender.ColorTo(sender.TextColor, Color.Red, c => sender.BackgroundColor = c, 2000),
+                sender.ColorTo(sender.TextColor, Color.Red, c => sender.TextColor = c, 2000),
+                sender.ScaleTo(.5, 2000),
+                sender.ColorTo(Color.Blue, sender.BackgroundColor, c => sender.BackgroundColor = c, 2000),
+                sender.ColorTo(Color.Red, sender.TextColor, c => sender.TextColor = c, 2000),
                 sender.ScaleTo(1, 2000)
                 );
-            sender.BackgroundColor = Color.Default;
-            sender.TextColor = Color.Default;
         }
     }
 }
---
+++ LayoutEx/LayoutEx/LimitRGBBehaviors.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LayoutEx
+{
+    public class LimitRGBBehaviors : Behavior<Entry>
+    {
+        public static readonly BindableProperty AttachBehaviorProperty =
+        BindableProperty.CreateAttached("AttachBehavior", typeof(bool),
+            typeof(LimitRGBBehaviors),
+            false, propertyChanged: OnAttachBehaviorChanged);
+        public static bool GetAttachBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(AttachBehaviorProperty);
+        }
+        public static void SetAttachBehavior(BindableObject view, bool value)
+        {
+            view.SetValue(AttachBehaviorProperty, value);
+        }
+
+        static void OnAttachBehaviorChanged(BindableObject view, object oldValue, object newValue)
+        {
+            var entry = view as Entry;
+            if (entry == null)
+            {
+                return;
+            }
+
+            bool attachBehavior = (bool)newValue;
+            if (attachBehavior)
+            {
+                entry.TextChanged += OnEntryTextChanged;
+            }
+            else
+            {
+                entry.TextChanged -= OnEntryTextChanged;
+            }
+        }
+
+        static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            int result;
+            bool isValid = int.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = (result<=255&&result>0) ? Color.Aqua : Color.Red;
+        }
+
+    }
+}
