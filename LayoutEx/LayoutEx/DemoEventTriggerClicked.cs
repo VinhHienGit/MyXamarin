6f1414d8c41c45e63568d545d9729a3706bfffb7
demorila <hoang.zn99@gmail.com>
2018-11-05 14:41:43 +0700

    create gitgnore

--- LayoutEx/LayoutEx/DemoEventTriggerClicked.cs
+++ LayoutEx/LayoutEx/DemoEventTriggerClicked.cs
@@ -11,13 +11,17 @@ namespace LayoutEx
         protected override  async void Invoke(Button sender)
         {
             await Task.WhenAll(
-                sender.ColorTo(sender.BackgroundColor, Color.Blue, c => sender.BackgroundColor = c, 2000),
-                sender.ColorTo(sender.TextColor, Color.Red, c => sender.TextColor = c, 2000),
-                sender.ScaleTo(.5, 2000),
-                sender.ColorTo(Color.Blue, sender.BackgroundColor, c => sender.BackgroundColor = c, 2000),
-                sender.ColorTo(Color.Red, sender.TextColor, c => sender.TextColor = c, 2000),
-                sender.ScaleTo(1, 2000)
+                sender.ScaleTo(.7, 2000),
+                sender.ColorTo(sender.BackgroundColor, Color.LightBlue, c => sender.BackgroundColor = c, 2000)
+                //sender.ColorTo(sender.TextColor, Color.Red, c => sender.TextColor = c, 2000)
                 );
+            await sender.ColorTo(sender.TextColor, Color.Red, c => sender.TextColor = c, 2000);
+            await Task.WhenAll(
+                sender.ScaleTo(1, 2000),
+                sender.ColorTo(sender.BackgroundColor, Color.Gray, c => sender.BackgroundColor = c, 2000)
+                //sender.ColorTo(sender.TextColor, Color.Gold, c => sender.TextColor = c, 2000)
+                );
+            await sender.ColorTo(sender.TextColor, Color.Gold, c => sender.TextColor = c, 2000);
         }
     }
 }
